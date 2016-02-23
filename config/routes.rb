7e75a0b38992a362b042b78538c16c63f62971a1
ShannonPaige <spaige@gmail.com>
2016-02-22 22:02:13 -0700

    Add routes for find and find_by for customers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :customers,     only: [:index, :show], defaults: {format: :json}
+      resources :customers,     only: [:index, :show], defaults: {format: :json} do
+        collection do
+          get '/find',      to: 'customers#show'
+          get '/find_all',  to: 'customers#index'
+        end
+      end
       resources :merchants,     only: [:index, :show], defaults: {format: :json}
       resources :items,         only: [:index, :show], defaults: {format: :json}
       resources :invoices,      only: [:index, :show], defaults: {format: :json}
@@ -10,5 +15,5 @@ Rails.application.routes.draw do
       resources :transactions,  only: [:index, :show], defaults: {format: :json}
     end
   end
-  
+
 end
--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -2,58 +2,67 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::CustomersController, type: :controller do
 
+  let!(:customer1) { create(:customer) }
+  let!(:customer2) { create(:customer) }
+
   describe "GET #index" do
     it "returns http success" do
       get :index, format: :json
       expect(response).to have_http_status(:success)
     end
 
-    it "returns an array" do
+    it "returns an array of objects" do
       get :index, format: :json
       assert_kind_of Array, json_response
     end
 
-    it "returns the correct number of items" do
-      customer = create(:customer)
-      customer2 = create(:customer)
+    it "returns the correct number of objects" do
       get :index, format: :json
       assert_equal Customer.count, json_response.count
     end
 
-    it "returns the correct items" do
-      customer1 = create(:customer)
-      customer2 = create(:customer)
+    it "returns the correct objects" do
       get :index, format: :json
-      item1 = json_response.first
-      item2 = json_response.last
+      object1 = json_response.first
+      object2 = json_response.last
 
-      assert_equal customer1.id, item1["id"]
-      assert_equal customer1.first_name, item1["first_name"]
-      assert_equal customer1.last_name, item1["last_name"]
-      assert_equal customer2.id, item2["id"]
-      assert_equal customer2.first_name, item2["first_name"]
-      assert_equal customer2.last_name, item2["last_name"]
+      assert_equal customer1.id, object1["id"]
+      assert_equal customer1.first_name, object1["first_name"]
+      assert_equal customer1.last_name, object1["last_name"]
+      assert_equal customer2.id, object2["id"]
+      assert_equal customer2.first_name, object2["first_name"]
+      assert_equal customer2.last_name, object2["last_name"]
     end
   end
 
   describe "GET #show" do
     it "returns http success" do
-      customer = create(:customer)
       get :show, id: Customer.first.id, format: :json
       expect(response).to have_http_status(:success)
     end
 
-    it "returns one record" do
-      customer = create(:customer)
-      customer2 = create(:customer)
+    it "returns one object" do
       get :show, format: :json, id: Customer.first.id
       assert_kind_of Hash, json_response
     end
 
-    it "returns the correct item" do
-      customer = create(:customer)
-      customer2 = create(:customer)
+    it "returns the correct object" do
       get :show, format: :json, id: Customer.first.id
+      assert_equal Customer.first.id, json_response["id"]
+    end
+
+    it "finds the correct object when given the id as a parameter" do
+      get :show, format: :json, id: Customer.first.id
+      assert_equal Customer.first.id, json_response["id"]
+    end
+
+    it "finds the correct object when given the first name as a parameter" do
+      get :show, format: :json, first_name: Customer.first.first_name
+      assert_equal Customer.first.id, json_response["id"]
+    end
+
+    it "finds the correct object when given the last name as a parameter" do
+      get :show, format: :json, last_name: Customer.first.last_name
       assert_equal Customer.first.id, json_response["id"]
     end
   end
